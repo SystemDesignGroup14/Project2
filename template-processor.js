2de238a46e3cf7f3cff8e80bdc5dca57767c0b28
Ramgopalhyndavbollepalli <rbollep1@uncc.edu>
2024-02-02 15:25:55 -0500

    lint is working

--- template-processor.js
+++ template-processor.js
@@ -1,34 +1,26 @@
 'use strict';
-// Define the TemplateProcessor class
-function TemplateProcessor(template) {
-    // Store the template in an instance variable
-    this.template = template;
-  }
-  
-  // Define the fillIn method
-  TemplateProcessor.prototype.fillIn = function(dictionary) {
-    // Make a copy of the template
-    let filledTemplate = this.template;
-  
-    // Iterate over the keys of the dictionary
-    for (const key in dictionary) {
-      if (Object.prototype.hasOwnProperty.call(dictionary, key)) {
-      const regex = new RegExp(`{{${key}}}`, 'g');
-      filledTemplate = filledTemplate.replace(regex, dictionary[key]);
-    }
-  }
-  
-    // Replace any remaining {{key}} patterns with an empty string
-    filledTemplate = filledTemplate.replace(/{{\w+}}/g, '');
-  
-    // Return the filled-in template
-    return filledTemplate;
-  };
 
-    if (typeof module !== 'undefined') {
-      // If so, export the TemplateProcessor function
-      module.exports = TemplateProcessor;
-    } else {
-      // If not, we're in a browser environment, so attach TemplateProcessor to the window object
-      window.TemplateProcessor = TemplateProcessor;
+class TemplateProcessor {
+    constructor(template) {
+        this.template = template;
     }
+
+    fillIn(dictionary) {
+        let renderedString = this.template;
+
+        for (const [key, value] of Object.entries(dictionary)) {
+            renderedString = renderedString.replaceAll(`{{${key}}}`, value);
+        }
+
+        return renderedString.replaceAll(/{{.*}}/g, '');
+    }
+}
+
+// Example usage:
+const template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
+const dateTemplate = new TemplateProcessor(template);
+
+const dictionary = { month: 'July', day: '1', year: '2016' };
+const str = dateTemplate.fillIn(dictionary);
+
+console.log(str);
